6cdc92b21d0334b673d108d874976bf68c19f3cc
spunkyraks <34817593+spunkyraks@users.noreply.github.com>
2017-12-25 01:45:44 +0530

    xytherbot
    xytherbot first version.

---
+++ CHATBOT.py
@@ -0,0 +1,211 @@
+user_input=input("")
+x=str("Xythertech")
+y=str("xythertech")
+z=str("XYTHERTECH")      
+if x==(user_input):
+      print("yes..may I help you")
+if y==(user_input):
+      print("oops..its me!")
+if z==(user_input):
+      print("hmmm...")
+for user_input in range(1):
+    print("hi")
+def test():
+    return(x,y,z)
+    
+user_input1 = input("what is your name:")
+x=str(user_input1)
+y=str(".")
+z=str("")
+b1=str("xythertech")
+b2=str("Xythertech")
+b3=str("XYTHERTECH")
+if x==user_input1:
+    print ("good morning."+ user_input1)
+if y==user_input1:
+    print("oh..sorry!")
+if z==user_input1:
+    print("oops..sorry!")
+if b1==user_input1:
+    print("Duplicate name!")
+if b2==user_input1:
+    print("Duplicate name!")
+if b3==user_input1:
+    print("Duplicate name!")
+def test():
+    return (x,y,z,b1,b2,b3)
+
+
+user_input2 = input("what will I call you:")
+b=str("user_input2")
+b1=str("xythertech")
+b2=str("Xythertech")
+b3=str("XYTHERTECH")
+if user_input2==user_input2:
+    print("hmmm...")
+if b1==user_input2:
+    print("Duplicate name!")
+if b2==user_input2:
+    print("Duplicate name!")
+if b3==user_input2:
+    print("Duplicate name!")
+def test1():
+    return (b)
+
+
+
+user_input4 = input("nice to meet you..."+ user_input2)
+if user_input4==user_input2:
+    print ("Duplicate value!")
+
+
+
+user_input5 = input("")
+d=str("who are you")
+std=str("who made you")
+std1=str("fuck")
+std2=str("hello")
+std3=str("don't want to talk")
+std4=str("bye")
+std5=str("are you a robot")
+std6=str("what is your name")
+std7=str("where are you from")
+std8=str("hi")
+if d==user_input5:
+    print ("XYTHERBOT")
+if std1==user_input5:
+    print("mind your language")
+if std==user_input5:
+    print("Xytertech's group of peoples.")
+if std2==user_input5:
+    print("hello nice to meet you")
+if std3==user_input5:
+    print("Bye")
+if std4==user_input5:
+    print("Tata!Bye!")
+if std5==user_input5:
+    print("No I'm not just a robot. I'm a CHATTERBOT,i.e made by Rakesh Sharma(founder),XYTHERTECH Inc. ")
+if std6==user_input5:
+    print ("Xythertech: to infinity And Beyond")
+if std7==user_input5:
+    print("I'm from India")
+if std8==user_input5:
+    print("hi..."+ user_input2)
+def test5():
+    return(d,std,std1,std2,std3,std4,std5,std6,std7,std8)
+
+
+user_input6 = input("")
+e=str("who are you")
+stes=str("who made you")
+stes1=str("fuck")
+stes2=str("hello")
+stes3=str("don't want to talk")
+stes4=str("bye")
+stes5=str("are you a robot")
+stes6=str("what is your name")
+stes7=str("where are you from")
+stes8=str("hi")
+if e==user_input6:
+    print ("XYTHERBOT....... :)")
+if stes1==user_input6:
+    print("mind your language")
+if stes==user_input6:
+    print("Xytertech's group of peoples.")
+if stes2==user_input6:
+    print("hello nice to meet you")
+if stes3==user_input6:
+    print("Bye")
+if stes4==user_input6:
+    print("Tata!Bye!")
+if stes5==user_input6:
+    print("No I'm not just a robot. I'm a CHATTERBOT,i.e made by Rakesh Sharma(founder),XYTHERTECH Inc. ")
+if stes6==user_input6:
+    print ("Xythertech: to infinity And Beyond")
+if stes7==user_input6:
+    print("I'm from India")
+if stes8==user_input6:
+    print("hi..."+ user_input2)
+def test6():
+    return(e,stes,stes1,stes2,stes3,stes4,stes5,stes6,stes7,stes8)
+
+
+
+user_input7 = input("")
+f=str("who are you")
+ste=str("who made you")
+ste1=str("fuck")
+ste2=str("hello")
+ste3=str("don't want to talk")
+ste4=str("bye")
+ste5=str("are you a bot")
+ste6=str("what is your name")
+ste7=str("where are you from")
+ste8=str("hi")
+if f==user_input7:
+    print ("XYTHERBOT....... :)")
+if ste1==user_input7:
+    print("mind your language")
+if ste==user_input7:
+    print("Xytertech's group of peoples.")
+if ste2==user_input7:
+    print("hello nice to meet you")
+if ste3==user_input7:
+    print("Bye")
+if ste4==user_input7:
+    print("Tata!Bye!")
+if ste5==user_input7:
+    print("No I'm not just a robot. I'm a CHATTERBOT,i.e made by Rakesh Sharma(founder),XYTHERTECH Inc. ")
+if ste6==user_input7:
+    print ("Xythertech: to infinity And Beyond")
+if ste7==user_input4:
+    print("I'm from India")
+if ste8==user_input7:
+    print("hi..."+ user_input2)
+def test7():
+    return(f,ste,ste1,ste2,ste3,ste4,ste5,ste6,ste7,ste8)
+
+
+
+user_input8 = input("")
+x=str("what are you")
+y=str("are you a bot")
+f1=str("who are you")
+st=str("who made you")
+st1=str("fuck")
+st2=str("hello")
+st3=str("don't want to talk")
+st4=str("bye")
+st5=str("what is your name")
+st6=str("where are you from")
+st7=str("hi")
+if x==(user_input8):
+      print("I'm a chatterbot and it is my first version")
+if y==user_input8:
+    print("No I'm not just a robot. I'm a CHATTERBOT,i.e made by Rakesh Sharma(founder),XYTHERTECH Inc. ")
+if f1==user_input8:
+    print ("XYTHERBOT....... :)")
+if st1==user_input8:
+    print("mind your language")
+if st==user_input8:
+    print("Xytertech's group of peoples.")
+if st2==user_input8:
+    print("hello nice to meet you")
+if st3==user_input8:
+    print("Bye")
+if st4==user_input8:
+    print("Tata!Bye!")
+if st6==user_input8:
+    print ("Xythertech: to infinity And Beyond")
+if st7==user_input7:
+    print("hi..."+ user_input2)
+else:
+    print("session over ! cooming back to you soon..")
+def test9():
+    return(f1,st,st1,st2,st3,st4,st5,st6,st7)
+    
+
+
+
+
+
